987cea6a63572884b7050bca166ee4fc633b08f3
Kumkum555 <Kumkumkapkoti@gmail.com>
2024-07-05 15:12:52 +0530

    addition

---
+++ sumsub.c/addsub.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+
+int number1;
+int number2; 
+
+
+printf("enter the first number :");
+scanf("%d",&number1);
+
+
+printf("enter the second number :");
+scanf("%d",&number2);
+
+
+int sum=(number1+number2);
+
+printf("the sum of two number is : %d",sum);
+
+
+    return 0;
+}
