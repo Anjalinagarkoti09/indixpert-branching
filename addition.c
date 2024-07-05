7d3e9b4bfc8a46e9e714c3b97d0286a78545fbf0
nisha515 <nishamehtaa@example.com>
2024-07-05 15:38:16 +0530

    addition

--- addition.c
+++ addition.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
 
-int sum(int num1, int num2);
-
 int main()
 
 {
 int num1;
 int num2;
+int sum=0;
 
 printf("enter num1: ");
 scanf("%d" ,&num1);
@@ -14,14 +13,8 @@ scanf("%d" ,&num1);
 printf("enter num2: ");
 scanf("%d" ,&num2);
  
- int sum=(num1+num2);
+ sum=num1+num2;
  printf("the sum of two number is : %d" ,sum);
 
     return 0;
-}
-int sum(int num1 ,int num2)
-
-{
-return num1+num2;
-
 }
