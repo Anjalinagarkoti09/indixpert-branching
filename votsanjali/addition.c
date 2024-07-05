169770faacf3c3807bb2abb59c00276163c220d2
Anjalinagarkoti09 <anjalinagarkoti@gmail.com>
2024-07-05 15:00:23 +0530

    sum of two numbers

---
+++ votsanjali/addition.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+
+
+
+int main()
+{
+
+int firstnumber;
+int secondnumber;
+
+printf("enter first number :");
+scanf("%d",&firstnumber);
+
+printf("enter second  number :");
+scanf("%d",&secondnumber);
+
+int sum=(firstnumber+secondnumber);
+printf("the sum of two number is :%d",sum);
+
+
+    return 0;
+}
