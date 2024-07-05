6bb5acbbcc63cd06fb3e508a1a30bd9c46ca3c47
Kumkum555 <Kumkumkapkoti@gmail.com>
2024-07-05 15:58:22 +0530

    just check

--- sumsub.c/addiction.c
+++ sumsub.c/addiction.c
@@ -1,26 +1,18 @@
-#include<stdio.h>
-
-int sum (int num1 , int num2);
+#include <stdio.h>
+void addition();
 
 int main()
 {
 
-int number1;
-int number2; 
-
-
-printf("enter the first number :");
-scanf("%d",&number1);
-
-
-printf("enter the second number :");
-scanf("%d",&number2);
-
-
-int sum=(number1+number2);
-
-printf("the sum of two number is : %d",sum);
-
-
+    addition();
     return 0;
+}
+
+
+void addition()
+{
+    int a = 10;
+    int b = 32;
+    
+    printf("%d\n", a + b);
 }
